2df7663d055cb2178e6a6ddc02f2632c20f68208
vedm-industries <81390906+vedm-industries@users.noreply.github.com>
2024-09-06 13:26:08 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
@@ -28,12 +29,12 @@ async def test_project(dut):
     dut.ui_in.value = 25  # Should result in uo_out being 50
     await ClockCycles(dut.clk, 1)
     expected_output = 50
-    dut._log.info(f"ui_in = {dut.ui_in.value}, uo_out = {dut.uo_out.value}")
+    dut._log.info(f"ui_in = {dut.ui_in.value}, converted_voltage = {dut.converted_voltage.value}, uo_out = {dut.uo_out.value}")
     assert dut.uo_out.value == expected_output, f"Expected uo_out to be {expected_output}, but got {dut.uo_out.value}. Input was {dut.ui_in.value}"
 
     # Test with another value
     dut.ui_in.value = 45  # Should result in uo_out being 90
     await ClockCycles(dut.clk, 1)
     expected_output = 90
-    dut._log.info(f"ui_in = {dut.ui_in.value}, uo_out = {dut.uo_out.value}")
+    dut._log.info(f"ui_in = {dut.ui_in.value}, converted_voltage = {dut.converted_voltage.value}, uo_out = {dut.uo_out.value}")
     assert dut.uo_out.value == expected_output, f"Expected uo_out to be {expected_output}, but got {dut.uo_out.value}. Input was {dut.ui_in.value}"
