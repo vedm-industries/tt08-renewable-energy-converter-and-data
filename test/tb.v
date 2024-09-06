2abee7a7602251e33b4228a098b1d84541bc80e6
vedm-industries <81390906+vedm-industries@users.noreply.github.com>
2024-09-06 16:32:30 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -5,10 +5,7 @@ module tb ();
 
   // Testbench specific signals
   reg [7:0] ui_in;
-  reg [7:0] uio_in;
   wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
   reg clk;
   reg rst_n;
   reg ena;
@@ -17,10 +14,6 @@ module tb ();
   tt_um_vedm_industries dut (
     .ui_in(ui_in),
     .uo_out(uo_out),
-    .uio_in(uio_in),
-    .uio_out(uio_out),
-    .uio_oe(uio_oe),
-    .ena(ena),
     .clk(clk),
     .rst_n(rst_n)
   );
@@ -40,16 +33,12 @@ module tb ();
 
   // Stimulus generation
   initial begin
-    ena = 1; // Enable the system
     ui_in = 8'd0;
-    uio_in = 8'd0;
 
     // Test patterns
     #20 ui_in = 8'd25;  // Expected output: 50 (ui_in * 2)
-    #20 uio_in = 8'd30; 
 
     #100 ui_in = 8'd45; // Expected output: 90
-    #20 uio_in = 8'd255;
 
     #50 $finish;  // End simulation
   end
@@ -57,7 +46,7 @@ module tb ();
   // Monitor outputs for debugging
   initial begin
     $monitor("Time = %t, ui_in = %h, uo_out = %h, uio_in = %h, uio_out = %h, uio_oe = %b",
-             $time, ui_in, uo_out, uio_in, uio_out, uio_oe);
+             $time, ui_in, uo_out);
   end
   // VCD dump for waveform analysi
   initial begin
