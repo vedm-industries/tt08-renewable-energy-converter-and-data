6864b62a54095dfda189bd4652b4e7a55309dfe2
vedm-industries <81390906+vedm-industries@users.noreply.github.com>
2024-09-06 16:52:46 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -13,24 +13,23 @@ async def test_project(dut):
 
     # Reset
     dut.rst_n.value = 0
+    dut.ena.value = 0
     await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 2)
 
     # Test input 25 -> Expect output 50
     dut.ui_in.value = 25
+    dut.ena.value = 1
     await ClockCycles(dut.clk, 1)
+    dut.ena.value = 0
     expected_output = 50
     assert dut.uo_out.value == expected_output, f"Expected uo_out to be {expected_output}, but got {dut.uo_out.value}"
 
     # Test input 45 -> Expect output 90
     dut.ui_in.value = 45
+    dut.ena.value = 1
     await ClockCycles(dut.clk, 1)
     expected_output = 90
     assert dut.uo_out.value == expected_output, f"Expected uo_out to be {expected_output}, but got {dut.uo_out.value}"
 
-    # Test input 100 -> Expect output 200, which is truncated to fit in 8 bits (200 in 8 bits is fine)
-    dut.ui_in.value = 100
-    await ClockCycles(dut.clk, 1)
-    expected_output = 200
-    assert dut.uo_out.value == expected_output, f"Expected uo_out to be {expected_output}, but got {dut.uo_out.value}"
