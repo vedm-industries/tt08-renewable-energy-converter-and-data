f796fb8e67c3d6248ac71bfc75c912420e1dc7f9
vedm-industries <81390906+vedm-industries@users.noreply.github.com>
2024-09-06 16:19:44 -0500

    Update tt_um_vedm_industries.v

--- src/tt_um_vedm_industries.v
+++ src/tt_um_vedm_industries.v
@@ -1,15 +1,11 @@
 module tt_um_vedm_industries (
     input wire [7:0] ui_in,
     output wire [7:0] uo_out,
-    input wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input wire ena,
     input wire clk,
     input wire rst_n
 );
 
-    wire [7:0] converted_voltage;  // Internal wire
+    wire [7:0] converted_voltage;
 
     // Instantiate power converter
     power_converter pc (
