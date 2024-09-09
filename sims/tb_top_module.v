e6e67ca2c3b420bf5f8ea83204962dab66c4c5b1
vedm-industries <81390906+vedm-industries@users.noreply.github.com>
2024-09-09 12:56:47 -0500

    Update tb_top_module.v

--- sims/tb_top_module.v
+++ sims/tb_top_module.v
@@ -35,6 +35,20 @@ initial begin
     ena = 1;  // Ensure ena is set
     #50 rst_n = 1;  // Release reset after 50ns
 end
+`ifdef GL_TEST
+      .VPWR(1'b1),
+      .VGND(1'b0),
+`endif
+
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 
 // Stimulus
 initial begin
